d2900838fd95e89a60297a70b23bbd30bf9ecff3
Kenan Philips <philipskenan@gmail.com>
2016-10-18 15:47:56 -0700

    added navbar and carousel

---
+++ js/main.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+   var height = $(window).height();
+   var width = $(window).width();
+   $('.item img').css('height', height - 50 + 'px');
+   $('.item img').css('width', width + 'px');
+});
